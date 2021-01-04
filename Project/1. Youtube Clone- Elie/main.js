d3d5c4ba808e1e854d472a181c4f23570ba6bf75
Sihyun <44644821+Siihyun@users.noreply.github.com>
2021-01-05 00:38:46 +0900

    Markup practice upload
    2021-01-05
    HTML/CSS Markup Practice
    
    2. ad-banner
    3.google-search-result-item
    4.feature-box
    5.logo-in-header
    6.breadcrumb-and-pagination
    
    upload 완료

--- Project/1. Youtube Clone- Elie/main.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector(".info .metadata .moreBtn");
-const title = document.querySelector(".info .metadata .title");
-
-button.addEventListener("click", () => {
-  button.classList.toggle("clicked");
-  title.classList.toggle("clamp");
-});
